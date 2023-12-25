300d6335f1834bb0c3b0079eb2a407bdb16e2b71
Didarchik <umirovdidar2002@gmail.com>
2023-12-25 23:56:55 +0600

    upd working

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Card, Accordion, ProgressBar, Navbar, Container, Nav} from "react-bootstrap"
-import Header from './components/Header.jsx/Header';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 function App() {
   return (
