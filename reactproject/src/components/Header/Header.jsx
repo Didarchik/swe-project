e62b061553b89e2cca76218bfe318d8590bd11d1
Didarchik <umirovdidar2002@gmail.com>
2023-12-25 22:33:24 +0600

    fixed

--- reactproject/src/components/Header/Header.jsx
+++ reactproject/src/components/Header/Header.jsx
@@ -13,18 +13,18 @@ function Header() {
   }
   return (
     <div className='header'>
-      <Navbar expand='lg' data-bs-theme="light" expanded={toggle} style={{background: 'aqua'}}>
+      <Navbar expand='lg' data-bs-theme="light" expanded={toggle}>
         <Container>
           <Navbar.Brand href="#home" className="fs-3"><img src={logo}/></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={handle}/>
           <Navbar.Collapse>
           <Nav className='ms-auto'>
-            <Nav.Link href="#deets" className="fs-5 navLink">Home</Nav.Link>
-            <Nav.Link  href="#memes" className="fs-5 navLink">About Us</Nav.Link>
-            <Nav.Link href='#contact' className="fs-5 navLink">Contact</Nav.Link>
-            <Nav.Link href="#test" className='fs-5 navLink'>Testimonials</Nav.Link>
+            <Nav.Link href="#deets" className="fs-5 navLink">Негізгі</Nav.Link>
+            <Nav.Link  href="#memes" className="fs-5 navLink">Біз жайлы</Nav.Link>
+            <Nav.Link href='#contact' className="fs-5 navLink">Деректер</Nav.Link>
+            <Nav.Link href="#test" className='fs-5 navLink'>Тестиномдар</Nav.Link>
             <Nav.Link href='#hz' className="fs-5 navLink"><img src={MyPhoto} style={{width: 20, height: 20}}/></Nav.Link>
-            <Button variant='primary' className='btn1 fs-5'>Booking now</Button>
+            <Button variant='primary' className='btn1 fs-5'>Заказ беру</Button>
           </Nav>
           </Navbar.Collapse>
         </Container>
--- reactproject/src/components/Home/Home.jsx
+++ reactproject/src/components/Home/Home.jsx
@@ -2,17 +2,25 @@ import React from 'react'
 import style from './Home.module.scss'
 import {Card, Button} from "react-bootstrap"
 import foodPicture from "../../assets/img/food.webp"
+import ReactTyped from "react-typed"
 function Home() {
   return (
     <div className='home_page'>
           <Card className='card'>
-            <Card.Title className='cardTitle'>Your Favourite Food Delivered Hot & Fresh</Card.Title>
+            <Card.Title className='cardTitle'>Сіздің сүйікті тағамдарыңыз бізбен, Foodie-мен</Card.Title>
             <Card.Body>
-              <Card.Text className='cardText' style={{color: '#828282'}}>Healthy switcher chefs do all the prep work, like peeding, chopping & marinating, so you can cook a fresh food.</Card.Text>
-              <Button variant='alert' className='btn2'>Order Now</Button>
+              <Card.Text className='cardText' style={{color: '#828282'}}>
+              Foodie Pizza бұл {" "} 
+              <ReactTyped strings={["дәмді пицца", "тегін сусындар", "атмосфералық өлеңдер"]} typeSpeed={100} loop/>
+              </Card.Text>
+              <Button variant='alert' className='btn2 d-flex' >
+                <p>Тапсырыс беру</p>
+                <p>-></p>
+              </Button>
             </Card.Body>
           </Card>
-          <img src={foodPicture} />
+            <img src={foodPicture} />
+            <div className='backBlock'></div>
     </div>
   )
 }
