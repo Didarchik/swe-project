f34c2b7abd6f1c5d47fbac12749cc312e0ef88e4
Didarchik <umirovdidar2002@gmail.com>
2023-12-27 17:16:17 +0600

    adding 3 page, not finished

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -4,12 +4,14 @@ import {Button, Card, Accordion, ProgressBar, Navbar, Container, Nav} from "reac
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Info from './components/Info/Info';
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
       <About />
+      <Info />
     </div>
   );
 }
---
+++ reactproject/src/components/Info/Info.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import style from "./Info.module.scss"
+import {Card, Button} from "react-bootstrap"
+import pickMeal from "../../assets/pickMeal.png"
+import truckIcon from "../../assets/truck.png"
+import fingerIcon from "../../assets/finger.png"
+function Info() {
+  return (
+    <div className={`d-flex align-items-center flex-column ${style.info_block}`}>
+        <Card className={style.upperBlock}>
+            <Card.Title className={style.info}>Деректер</Card.Title>
+            <Card.Title className={style.title}>How it works</Card.Title>
+            <Card.Text className={style.cardText}>Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et elit. Dolor  turpis molestie dui magnis facilisis at fringilla quam.</Card.Text>
+        </Card>
+        <div className={`d-flex flex-wrap align-items-center justify-content-center ${style.lowerBlock}`}>
+            <Card className={style.items}>
+                <img src={pickMeal}/>
+                <Card.Title className={style.itemTitle}>Pick Meals</Card.Title>
+                <Card.Text className={style.itemText}>Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.</Card.Text>
+            </Card>
+            <Card className={style.items}>
+                <img src={fingerIcon}/>
+                <Card.Title className={style.itemTitle}>Pick Meals</Card.Title>
+                <Card.Text className={style.itemText}>Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.</Card.Text>
+            </Card>
+            <Card className={style.items}>
+                <img src={truckIcon} />
+                <Card.Title className={style.itemTitle}>Pick Meals</Card.Title>
+                <Card.Text className={style.itemText}>Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.</Card.Text>
+            </Card>
+        </div>
+    </div>
+  )
+}
+
+export default Info
